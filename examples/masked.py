5918be32946630bdd86f96e13fd4a69407ebee36
nurettin senyer <seyyah@bil.omu.edu.tr>
2016-02-28 00:28:40 +0200

    mevalana sozlerinden semazen çizimi

--- examples/masked.py
+++ examples/masked.py
@@ -15,20 +15,20 @@ from wordcloud import WordCloud, STOPWORDS
 d = path.dirname(__file__)
 
 # Read the whole text.
-text = open(path.join(d, 'alice.txt')).read()
+text = open(path.join(d, 'sozler3.txt')).read()
 
 # read the mask image
 # taken from
 # http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
-alice_mask = np.array(Image.open(path.join(d, "alice_mask.png")))
+alice_mask = np.array(Image.open(path.join(d, "mask3.png")))
 
 wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
-               stopwords=STOPWORDS.add("said"))
+               stopwords=STOPWORDS.add("yorulunca"))
 # generate word cloud
 wc.generate(text)
 
 # store to file
-wc.to_file(path.join(d, "alice.png"))
+wc.to_file(path.join(d, "mask_output3.png"))
 
 # show
 plt.imshow(wc)
